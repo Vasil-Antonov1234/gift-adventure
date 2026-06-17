431b453820bb0cc668b0d16824da5fdcee08ddfb
Vasil-Antonov1234 <vaskoantonov1234@gmail.com>
2026-06-18 00:02:07 +0300

    Add http server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log("working...")
+import http from "http";
+
+const server = http.createServer((req, res) => {
+    res.write("Test");
+
+    res.end();
+});
+
+server.listen(5000, () => console.log("Server is listening on http://localhost:5000..."));
