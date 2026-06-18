bac282032061805928939e643b211ff679022b69
Vasil-Antonov1234 <vaskoantonov1234@gmail.com>
2026-06-18 11:55:15 +0300

    Update index.js
    Add site.css styles to response

--- src/index.js
+++ src/index.js
@@ -2,11 +2,28 @@ import http from "http";
 import fs from "fs/promises";
 
 const server = http.createServer(async (req, res) => {
+
+    if (req.url.endsWith(".css")) {
+
+        const tokens = req.url.split("/");
+        const file = tokens[tokens.length - 1];
+
+        try {
+            const response = await fs.readFile(`./src/content/styles/${file}`, "utf-8");
+            res.writeHead(200, { "content-type": "text/css" });
+    
+            res.write(response);
+            return res.end();
+        } catch (error) {
+            console.log(error.message);
+        }
+    };
+
     const homePage = await fs.readFile("./src/views/index.html", "utf-8");
 
     res.writeHead(200, { "content-type": "text/html" });
     res.write(homePage);
-    
+
     res.end();
 });
 
