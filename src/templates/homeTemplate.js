4b5dae704cd988bc326ffe0efccc38bc15a339fc
Vasil-Antonov1234 <vaskoantonov1234@gmail.com>
2026-06-18 23:16:45 +0300

    Update homeTemplate.js

--- src/templates/homeTemplate.js
+++ src/templates/homeTemplate.js
@@ -1,17 +1,17 @@
 import fs from "fs/promises";
 
 export async function renderHome() {
-    
+
     try {
         let homePage = await fs.readFile("./src/views/index.html", "utf-8");
         const dataJson = await fs.readFile("./src/data/data.json", "utf-8");
         const data = JSON.parse(dataJson);
 
         function adventureTemplate(adventure) {
-            
+
             return `
                 <li class="adventure-card">
-                    <img src=${adventure.imageUrl} alt="Cappadocia">
+                    <img src=${adventure.imageUrl} alt=${adventure.name}>
                     <h3>${adventure.name}</h3>
                     <p><span>Location: </span>${adventure.location}</p>
                     <p><span>Description: </span>${adventure.description}</p>
@@ -21,27 +21,13 @@ export async function renderHome() {
                     </ul>
                 </li>
             `
-        } 
+        }
 
+        return homePage.replace("{{adventures}}", data.map((adventure) => adventureTemplate(adventure)).join("\n"));
 
-        const adventures = []; 
-
-        data.map((adventure) => {
-            adventures.push(
-                adventureTemplate(adventure)
-            )
-        })
-
-        const result = adventures.join("\n");
-
-        homePage = homePage.replace("{{adventures}}", result);
-
-        return homePage;
-
-        
     } catch (error) {
         alert(error.message)
     }
 
-    
+
 }
